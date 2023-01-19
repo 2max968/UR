b437a945c6dc0c4724e2691aed30415228c5ad63
2max968 <maximilian-haag@web.de>
2023-01-19 14:22:29 +0100

    Lüfter rotierend gemacht

---
+++ Assets/Scripts/AnimRotate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimRotate : MonoBehaviour
+{
+    public float RPM = 30;
+    public Vector3 RotationAxis = new Vector3(0, 1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Time.deltaTime * 360 * RPM / 60;
+        transform.Rotate(RotationAxis, angle, Space.Self);
+    }
+}
